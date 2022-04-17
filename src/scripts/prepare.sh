b3b8c332d56c24735d920bcc5c0a3ff7e9a751fc
mkusaka <hinoshita1992@gmail.com>
2022-04-17 14:38:56 +0900

    fix scripts

--- src/scripts/prepare.sh
+++ src/scripts/prepare.sh
@@ -2,4 +2,7 @@
 
 # Fetch remote revision to get diff of target revisions
 # This process needs if repository shallow cloned.
+echo "$FILTER_REMOTE"
+echo "$FILTER_BASE_REVISION"
+echo "$FILTER_HEAD_REVISION"
 git fetch "$FILTER_REMOTE" "$FILTER_BASE_REVISION" "$FILTER_HEAD_REVISION"
