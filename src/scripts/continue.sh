69b36b09f41c7a027afca23bddfe801ce2bcb5d9
mkusaka <hinoshita1992@gmail.com>
2022-04-17 13:45:09 +0900

    fix scripts

--- src/scripts/continue.sh
+++ src/scripts/continue.sh
@@ -1,6 +1,15 @@
 #!/bin/bash -e
 
-if [ "$FILTER_PATH" != "" ] && [ $(git diff "$FILTER_BASE_REVISION".."$FILTER_HEAD_REVISION" --name-only | grep -E "$FILTER_PATH") ]; then
+if [ -n "$FILTER_ORB_DEBUG" ]; then
+  set -x
+fi
+
+if [ -z "$FILTER_PATH" ]; then
+  echo no filter path given
+  exit 1
+fi
+
+if ! git diff "$FILTER_BASE_REVISION".."$FILTER_HEAD_REVISION" --name-only | grep -q -E "$FILTER_PATH"; then
   echo ci canceled due to diff detected to "$FILTER_PATH"
   circleci-agent step halt
 fi
