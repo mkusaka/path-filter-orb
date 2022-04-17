0be873746234d74542cbb8737fe05b45c6ccb1be
mkusaka <hinoshita1992@gmail.com>
2022-04-17 15:19:29 +0900

    improve test

--- src/scripts/stop.sh
+++ src/scripts/stop.sh
@@ -10,6 +10,6 @@ if [ -z "$FILTER_PATH" ]; then
 fi
 
 if git diff "$FILTER_BASE_REVISION".."$FILTER_HEAD_REVISION" --name-only | grep -q -E "$FILTER_PATH"; then
-  echo ci canceled due to diff detected to "$FILTER_PATH"  between "$FILTER_BASE_REVISION".."$FILTER_HEAD_REVISION"
+  echo ci canceled due to diff detected to "$FILTER_PATH" between "$FILTER_BASE_REVISION".."$FILTER_HEAD_REVISION"
   circleci-agent step halt
 fi
